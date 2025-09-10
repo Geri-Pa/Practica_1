802cda9b8ddade958fb8f4e9efdd5c023f5b8f95
avinautt12 <avinaluciano50@gamil.com>
2025-09-10 12:43:42 -0600

    Cambios

---
+++ app/src/main/java/com/example/practica_1/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.practica_1
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import com.example.practica_1.ui.theme.Practica_1Theme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica_1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    Button(
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Composable
+fun Button(modifier: Modifier = Modifier){
+    Text(
+        text = "tilin",
+        modifier = modifier
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practica_1Theme {
+        Greeting("Android")
+    }
+}
